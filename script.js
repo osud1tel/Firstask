fab7e864615a95267031022dc085000a9d7060d6
osud1tel <90594121+osud1tel@users.noreply.github.com>
2021-09-13 08:52:24 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function count(firstNumber, secondNumber, sum) {
+    firstNumber = prompt('Введите первое число');
+    secondNumber = prompt('Введите второе число');
+    sum = (firstNumber/1) + (secondNumber/1);
+    return sum;
+}
+alert(count());
